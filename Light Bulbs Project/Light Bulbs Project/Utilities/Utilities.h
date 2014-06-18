1c133e7ca86729629c9fc3364579bed829b1299c
samthui <Mau vi ch, vi vng ln vi ch>
2014-06-19 04:27:32 +0700

    Done base UI
    Missing commit

---
+++ Light Bulbs Project/Light Bulbs Project/Utilities/Utilities.h
@@ -0,0 +1,20 @@
+#import <Foundation/Foundation.h>
+#import <CoreBluetooth/CoreBluetooth.h>
+
+#import "Defines.h"
+#import "Constants.h"
+
+typedef struct {
+    short index;
+    BOOL found;
+}CheckTableItem;
+
+@interface Utilities : NSObject
+
++(NSString*) UUIDofPeripheral:(CBPeripheral*)peripheral;
+
++(NSMutableArray*) arrayFromUserDefaultWithKey:(NSString*)key;
++(void) saveToUserDefaultWithKey:(NSString*) key forArray:(NSMutableArray*)array;
++(CheckTableItem) findSavedObject:(NSString *)UUID;
+
+@end
